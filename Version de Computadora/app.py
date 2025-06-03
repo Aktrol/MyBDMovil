5f6d3b01e454d24bd0b5a0eaf8637bb0b61acd87
Aktrol <trol88xd@gmail.com>
2025-06-02 18:27:30 -0600

    Error al documentar codigo

--- Version de Computadora/app.py
+++
@@ -1,6 +0,0 @@
-#carpeta importada donde esta el AppControler
-from controller.app_controller import AppController
-#si la funcion name tiene la funcion main hacer 
-if __name__ == "__main__":
-    #crea la app y la ejecuta con la funcion Appcontroler 
-    app = AppController()
